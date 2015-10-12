3dea9e47e483773f7bdc5727b7fb863942d70cbb
ne_Sachirou <utakata.c4se@gmail.com>
2015-10-12 22:36:58 +0900

    Error on invalid project name.

--- src/Lib.hs
+++ src/Lib.hs
@@ -9,10 +9,14 @@ module Lib
     ) where
 
 import Literal ( literalFile )
+import Text.Regex.TDFA
 
 cmdNew :: [String] -> IO ()
-cmdNew args =
-  putStrLn "NEW"
+cmdNew args = let projectName:_ = args
+  in do
+    case validateProjectName projectName of
+      Right projectName -> putStrLn projectName
+      Left error        -> fail error
 
 cmdRm :: [String] -> IO ()
 cmdRm args =
@@ -31,5 +35,9 @@ cmdPath args =
   putStrLn "PATH"
 
 cmdHelp :: IO ()
-cmdHelp =
-  putStrLn [literalFile|src/Help.txt|]
+cmdHelp = putStrLn [literalFile|src/Help.txt|]
+
+validateProjectName :: String -> Either String String
+validateProjectName projectName
+  | (projectName =~ "^[-A-Za-z0-9_]+$" :: Int) == 0 = Left "The project name shold only contain [-A-Za-z0-9_]"
+  | otherwise                                       = Right projectName
