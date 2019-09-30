8505c9661d827b5b2e5533460e8221000f1ec324
ne-sachirou <ne-sachirou@hatena.ne.jp>
2019-09-30 13:44:25 +0900

    Fix `rubocop`

--- features/step_definitions/command_steps.rb
+++ features/step_definitions/command_steps.rb
@@ -10,41 +10,41 @@ After do
   FileUtils.rm_rf "#{__dir__}/../tmp", secure: true
 end
 
-Given /^run NEW command with an option "([^"]*)"$/ do |project|
+Given(/^run NEW command with an option "([^"]*)"$/) do |project|
   step %(I run `private-values new #{project}`)
 end
 
-Given /^run RM command with an option "([^"]*)"$/ do |project|
+Given(/^run RM command with an option "([^"]*)"$/) do |project|
   step %(I run `private-values rm #{project}`)
 end
 
-Given /^run PROJECTS command/ do
+Given(/^run PROJECTS command/) do
   step %(I run `private-values projects`)
 end
 
-Given /^run KEYS command with an option "([^"]*)"$/ do |project|
+Given(/^run KEYS command with an option "([^"]*)"$/) do |project|
   step %(I run `private-values keys #{project}`)
 end
 
-Given /^run SET command with options "([^"]*)" and "([^"]*)"$/ do |arg1, value|
+Given(/^run SET command with options "([^"]*)" and "([^"]*)"$/) do |arg1, value|
   step %(I run `private-values set #{arg1} #{value}`)
 end
 
-Given /^run GET command with an option "([^"]*)"$/ do |arg1|
+Given(/^run GET command with an option "([^"]*)"$/) do |arg1|
   step %(I run `private-values get #{arg1}`)
 end
 
-Given /^run PATH command with an option "([^"]*)"$/ do |project|
+Given(/^run PATH command with an option "([^"]*)"$/) do |project|
   step %(I run `private-values path #{project}`)
 end
 
-Given /^using "([^"]*)" for a rc file$/ do |rc_file|
+Given(/^using "([^"]*)" for a rc file$/) do |rc_file|
   FileUtils.cp "#{__dir__}/../../fixtures/#{rc_file}",
                "#{__dir__}/../tmp/private-values.rc",
                preserve: true
 end
 
-Given /^a project named "([^"]*)"$/ do |project|
+Given(/^a project named "([^"]*)"$/) do |project|
   step %(using "private-values.default.rc" for a rc file)
   step %(run NEW command with an option "#{project}")
 end
