e6b289244cdc4c8ebfd9e983a7652d48e90e5e90
ne-sachirou <ne-sachirou@hatena.ne.jp>
2019-09-30 13:39:24 +0900

    `rubocop -a --safe`

--- Rakefile
+++ Rakefile
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'cucumber'
 require 'cucumber/rake/task'
 require 'erubis'
@@ -9,10 +11,10 @@ end
 
 desc 'Build this project'
 task :build do
-  readme = Erubis::Eruby.new(File.read("#{__dir__}/src/README.md.erb", mode: 'r:utf-8')).result({
-    help: File.read("#{__dir__}/src/Help.txt", mode: 'r:utf-8').strip,
-  })
-  File.open("#{__dir__}/README.md", 'w:utf-8'){|f| f.write readme }
+  readme = Erubis::Eruby.new(File.read("#{__dir__}/src/README.md.erb", mode: 'r:utf-8')).result(
+    help: File.read("#{__dir__}/src/Help.txt", mode: 'r:utf-8').strip
+  )
+  File.open("#{__dir__}/README.md", 'w:utf-8') { |f| f.write readme }
   sh 'stack build'
   FileUtils.mkdir 'bin' unless File.exist? 'bin'
   FileUtils.cp `stack exec which private-values`.chomp, 'bin/', preserve: true
--- features/step_definitions/command_steps.rb
+++ features/step_definitions/command_steps.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require 'fileutils'
 
 Before do
@@ -9,31 +11,31 @@ After do
 end
 
 Given /^run NEW command with an option "([^"]*)"$/ do |project|
-  step %{I run `private-values new #{project}`}
+  step %(I run `private-values new #{project}`)
 end
 
 Given /^run RM command with an option "([^"]*)"$/ do |project|
-  step %{I run `private-values rm #{project}`}
+  step %(I run `private-values rm #{project}`)
 end
 
 Given /^run PROJECTS command/ do
-  step %{I run `private-values projects`}
+  step %(I run `private-values projects`)
 end
 
 Given /^run KEYS command with an option "([^"]*)"$/ do |project|
-  step %{I run `private-values keys #{project}`}
+  step %(I run `private-values keys #{project}`)
 end
 
 Given /^run SET command with options "([^"]*)" and "([^"]*)"$/ do |arg1, value|
-  step %{I run `private-values set #{arg1} #{value}`}
+  step %(I run `private-values set #{arg1} #{value}`)
 end
 
 Given /^run GET command with an option "([^"]*)"$/ do |arg1|
-  step %{I run `private-values get #{arg1}`}
+  step %(I run `private-values get #{arg1}`)
 end
 
 Given /^run PATH command with an option "([^"]*)"$/ do |project|
-  step %{I run `private-values path #{project}`}
+  step %(I run `private-values path #{project}`)
 end
 
 Given /^using "([^"]*)" for a rc file$/ do |rc_file|
@@ -43,6 +45,6 @@ Given /^using "([^"]*)" for a rc file$/ do |rc_file|
 end
 
 Given /^a project named "([^"]*)"$/ do |project|
-  step %{using "private-values.default.rc" for a rc file}
-  step %{run NEW command with an option "#{project}"}
+  step %(using "private-values.default.rc" for a rc file)
+  step %(run NEW command with an option "#{project}")
 end
