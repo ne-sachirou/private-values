f739bdf1e3d5a2a094a6380aef62a9e4885eb966
ne-sachirou <ne-sachirou@hatena.ne.jp>
2020-07-07 13:16:58 +0900

    Fix deprication

--- src/YamlUtil.hs
+++ src/YamlUtil.hs
@@ -2,11 +2,9 @@ module YamlUtil
   ( decodeYamlFile
   ) where
 
-import Data.ByteString as ByteString ( readFile )
-import Data.Maybe ( fromMaybe )
-import Data.Yaml ( decode, object, Value )
+import Data.Yaml ( decodeFileThrow, Value )
 
 decodeYamlFile :: String -> IO Value
 decodeYamlFile path =
-  do yaml <- ByteString.readFile path
-     return $ fromMaybe (object []) (decode yaml :: Maybe Value)
+  do value <- decodeFileThrow path
+     return value
